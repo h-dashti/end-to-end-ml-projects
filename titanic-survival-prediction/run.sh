8459b51ce5d1cf7b592963a553eb843be199e4f7
Nick Dashti <ndashti@spartan-login3.hpc.unimelb.edu.au>
2025-03-26 17:16:12 +1100

    .

---
+++ titanic-survival-prediction/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --time=02:00:00          
+#SBATCH --nodes=1
+#SBATCH --ntasks=1              
+#SBATCH --cpus-per-task=16      
+#SBATCH --mem=16G                
+#SBATCH --partition=sapphire      
+
+module load Python
+source ~/.venvs/env_ml/bin/activate
+
+
+python run_xgb_gridsearch.py
